a6b0da5a22467ed37784f5e9981cabe1f35479d0
xkhusan <xkhusan@gmail.com>
2025-12-13 10:39:44 +0500

    fix

--- frontend/client-app/src/lib/config.ts
+++ frontend/client-app/src/lib/config.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://172.174.116.55/';
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? 'https://fast-forward-apty.onrender.com';
 
 export const DEFAULT_USERNAME = process.env.NEXT_PUBLIC_DEFAULT_USERNAME ?? 'khasanrashidov';
