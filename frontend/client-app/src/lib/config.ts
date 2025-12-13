af3cbdf7676a2e7ea6cf34628634b3153e6fcdc9
xkhusan <xkhusan@gmail.com>
2025-12-13 10:16:25 +0500

    reversed

--- frontend/client-app/src/lib/config.ts
+++ frontend/client-app/src/lib/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = '';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://172.174.116.55/';
 
 export const DEFAULT_USERNAME = process.env.NEXT_PUBLIC_DEFAULT_USERNAME ?? 'khasanrashidov';
